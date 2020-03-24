55a9c0adc950a6904fb751155ef5df36a5f313b9
JackeyVictory <48275062+JackeyVictory@users.noreply.github.com>
2020-03-24 08:15:25 +0800

    Update q1.py

--- q1.py
+++ q1.py
@@ -17,6 +17,29 @@
 
 '''
 
-def how_many_ways(digitarray):
-    # implement here
-    
+def  how_many_ways(digitarray):
+    length = len(digitarray)
+    if digitarray[0] == '0':
+        return 0
+    f = {}
+    f[0], f[1] = 1, 1
+    for l in range(2, length + 1):
+        x = int(digitarray[l - 2:l])
+
+        if digitarray[l - 1] == '0':
+            if digitarray[l - 2] == '0' or x > 26:
+                return 0
+            else:
+                f[l] = f[l - 2]
+        elif digitarray[l - 2] == '0' or x > 26:
+            f[l] = f[l - 1]
+        else:
+            f[l] = f[l - 1] + f[l - 2]
+
+    return f[length]
+while True:
+    try:
+        s = input()
+        print(how_many_ways(s))
+    except:
+        break
